25decddb813cc295a6c43eead9a03c384d86f83d
KisonHe <kisonmaxine@gmail.com>
2025-12-01 11:41:17 +0800

    简化部份代码

--- example/base-ez-control-websocket.rs
+++ example/base-ez-control-websocket.rs
@@ -4,12 +4,9 @@
 // https://github.com/orgs/hexfellow/discussions/2
 
 use clap::Parser;
-use futures_util::{SinkExt, StreamExt};
+use futures_util::StreamExt;
 use log::{error, info, warn};
-use prost::Message;
-use robot_demos::{
-    decode_message, decode_websocket_message, proto_public_api, ACCEPTABLE_PROTOCOL_MAJOR_VERSION,
-};
+use robot_demos::{decode_websocket_message, proto_public_api, send_api_down_message_to_websocket};
 use std::net::SocketAddrV4;
 use tokio_tungstenite::MaybeTlsStream;
 
@@ -44,78 +41,61 @@ async fn main() {
         _ => warn!("set_nodelay not implemented for TLS streams"),
     }
     let (mut ws_sink, mut ws_stream) = ws_stream.split();
+
     // Spawn the print task
     tokio::spawn(async move {
         while let Some(Ok(msg)) = ws_stream.next().await {
-            match msg {
-                tungstenite::Message::Binary(bytes) => {
-                    let msg = proto_public_api::ApiUp::decode(bytes).unwrap();
-                    if let Some(log) = msg.log {
-                        warn!("Log from base: {:?}", log); // Having a log usually means something went boom, so lets print it.
-                    }
-                    if msg.protocol_major_version != ACCEPTABLE_PROTOCOL_MAJOR_VERSION {
-                        warn!(
-                            "Protocol major version is not {}, current version: {}. This might cause compatibility issues. Consider upgrading the base firmware.",
-                            ACCEPTABLE_PROTOCOL_MAJOR_VERSION, msg.protocol_major_version
-                        );
-                        // If protocol major version does not match, lets just stop printing odometry.
-                        return;
-                    }
-                    match msg.status {
-                        Some(proto_public_api::api_up::Status::BaseStatus(base_status)) => {
-                            if let Some(estimated_odometry) = base_status.estimated_odometry {
-                                info!("Estimated odometry: {:?}", estimated_odometry);
-                            }
-                        }
-                        _ => {}
+            let msg = decode_websocket_message(msg).unwrap();
+            match msg.status {
+                Some(proto_public_api::api_up::Status::BaseStatus(base_status)) => {
+                    if let Some(estimated_odometry) = base_status.estimated_odometry {
+                        info!("Estimated odometry: {:?}", estimated_odometry);
                     }
                 }
                 _ => {}
-            };
+            }
         }
     });
+
     // Down, base command, command, api_control_initialize = true
     // Set report frequency to 50Hz; Since its a simple demo using simple_move_command, we don't need to hear from base too often.
     // If not changed, it will spam Estimated odometry at 1000Hz, which is too much for a simple demo.
     // This will only work for the current session, different sessions have independent report frequency settings.
-    ws_sink
-        .send(tungstenite::Message::Binary(
-            proto_public_api::ApiDown {
-                down: Some(proto_public_api::api_down::Down::SetReportFrequency(
-                    proto_public_api::ReportFrequency::Rf50Hz as i32,
-                )),
-            }
-            .encode_to_vec()
-            .into(),
-        ))
-        .await
-        .expect("Failed to send set report frequency message");
+    send_api_down_message_to_websocket(
+        &mut ws_sink,
+        proto_public_api::ApiDown {
+            down: Some(proto_public_api::api_down::Down::SetReportFrequency(
+                proto_public_api::ReportFrequency::Rf50Hz as i32,
+            )),
+        },
+    )
+    .await
+    .expect("Failed to send set report frequency message");
 
     // Before sending move command, we need to set initialize the base first.
-    ws_sink
-        .send(tungstenite::Message::Binary(
-            proto_public_api::ApiDown {
-                down: Some(proto_public_api::api_down::Down::BaseCommand(
-                    proto_public_api::BaseCommand {
-                        command: Some(
-                            proto_public_api::base_command::Command::ApiControlInitialize(true),
-                        ),
-                    },
-                )),
-            }
-            .encode_to_vec()
-            .into(),
-        ))
-        .await
-        .expect("Failed to send initialize message");
+    send_api_down_message_to_websocket(
+        &mut ws_sink,
+        proto_public_api::ApiDown {
+            down: Some(proto_public_api::api_down::Down::BaseCommand(
+                proto_public_api::BaseCommand {
+                    command: Some(
+                        proto_public_api::base_command::Command::ApiControlInitialize(true),
+                    ),
+                },
+            )),
+        },
+    )
+    .await
+    .expect("Failed to send initialize message");
     let start_time = std::time::Instant::now();
     while start_time.elapsed() < std::time::Duration::from_secs(10) {
         // You can also use tokio's tick if you want
         tokio::time::sleep(std::time::Duration::from_millis(20)).await;
 
         // Down, base command, command, simple_move_command, vx = 0.0, vy = 0, w = 0.1
-        ws_sink
-            .send(tungstenite::Message::Binary(proto_public_api::ApiDown {
+        send_api_down_message_to_websocket(
+            &mut ws_sink,
+            proto_public_api::ApiDown {
                 down: Some(proto_public_api::api_down::Down::BaseCommand(
                     proto_public_api::BaseCommand {
                         command: Some(proto_public_api::base_command::Command::SimpleMoveCommand(
@@ -133,26 +113,26 @@ async fn main() {
                         )),
                     },
                 )),
-            }.encode_to_vec().into()))
-            .await.expect("Failed to send move message");
+            },
+        )
+        .await
+        .expect("Failed to send move message");
     }
     // This is essential because if base lost control for a long time, it will enter protected state.
     // So lets tell the base we are finishing our control session.
-    ws_sink
-        .send(tungstenite::Message::Binary(
-            proto_public_api::ApiDown {
-                down: Some(proto_public_api::api_down::Down::BaseCommand(
-                    proto_public_api::BaseCommand {
-                        command: Some(
-                            proto_public_api::base_command::Command::ApiControlInitialize(false),
-                        ),
-                    },
-                )),
-            }
-            .encode_to_vec()
-            .into(),
-        ))
-        .await
-        .expect("Failed to send deinitialize message");
+    send_api_down_message_to_websocket(
+        &mut ws_sink,
+        proto_public_api::ApiDown {
+            down: Some(proto_public_api::api_down::Down::BaseCommand(
+                proto_public_api::BaseCommand {
+                    command: Some(
+                        proto_public_api::base_command::Command::ApiControlInitialize(false),
+                    ),
+                },
+            )),
+        },
+    )
+    .await
+    .expect("Failed to send deinitialize message");
     info!("Successfully deinitialized base");
 }
